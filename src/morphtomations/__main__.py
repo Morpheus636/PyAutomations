a48960ffa63fcee2edab8a5fbb5ed127e33a24fb
Josh Levin <floydaroo@gmail.com>
2022-03-09 16:40:04 -0500

    Created keep_alive

---
+++ src/morphtomations/__main__.py
@@ -0,0 +1,5 @@
+from .keep_alive import keep_alive
+
+keep_alive()
+
+# Do the thing.
---
+++ src/morphtomations/keep_alive.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from threading import Thread
+
+# this is only used for replit and does not need to be added if you are running this client-side
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "Online and pinging"
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
